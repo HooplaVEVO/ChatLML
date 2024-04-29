42ccc68b745519773fdd9e6d25f217961cd73358
lailahamdan1116 <94996494+lailahamdan1116@users.noreply.github.com>
2024-04-29 14:43:48 -0500

    Update client.cpp

--- ChatLML/client.cpp
+++ ChatLML/client.cpp
@@ -1,3 +1,5 @@
+#define PORT 8080
+
 #include <iostream>
 #include <cstring>
 #include <string>
@@ -5,51 +7,48 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-
 #include "client.h"
+#include "server.h"
 
-#define PORT 8080
-
-    Client::Client(std::string& user){
-        Client->username=user;
-    }
+Client::Client(std::string& user) : username(user), sock(-1) {
+    server_address.sin_family = AF_INET;  // Setting up default values
+    server_address.sin_port = htons(PORT);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+}
 
     int Client::init(std::string IP) {
-        // Create socket
-        if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-            std::cerr << "Socket creation error" << std::endl;
-            exit(EXIT_FAILURE);
-        }
+    // Create socket
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        std::cerr << "Socket creation error" << std::endl;
+        exit(EXIT_FAILURE);
+    }
 
         // Initialize server address
-        server_address.sin_family = AF_INET;
-        server_address.sin_port = htons(PORT);
-        if (inet_pton(AF_INET, IP, &server_address.sin_addr) <= 0) {
-            std::cerr << "Invalid address/ Address not supported" << std::endl;
-            exit(EXIT_FAILURE);
-        }
-
-        // Connect to server
-        if (connect(sock, (struct sockaddr*)&server_address, sizeof(server_address)) < 0) {
-            std::cerr << "Connection Failed" << std::endl;
-            exit(EXIT_FAILURE);
-        }
-        return 0;
+        if (inet_pton(AF_INET, IP.c_str(), &server_address.sin_addr) <= 0) {
+        std::cerr << "Invalid address/ Address not supported" << std::endl;
+        exit(EXIT_FAILURE);
     }
 
-    void Client::send_message(std::string message) {
-        std::string messageWithUsername = username; // Add username to the message
-        messageWithUsername += ": ";
-        messageWithUsername += message;
-        send(sock, messageWithUsername.c_str(), messageWithUsername.length(), 0);
+      // Connect to server
+    if (connect(sock, (struct sockaddr*)&server_address, sizeof(server_address)) < 0) {
+        std::cerr << "Connection Failed" << std::endl;
+        exit(EXIT_FAILURE);
     }
+    return 0;
+}
+
+void Client::send_message(const std::string& message) {
+    std::string messageWithUsername = username + ": " + message;
+    send(sock, messageWithUsername.c_str(), messageWithUsername.length(), 0);
+}
 
     std::string Client::receive_message() {
-        char buffer[1024] = { 0 };
-        read(sock, buffer, 1024);
-        return std::string(buffer);
-    }
+    char buffer[1024] = {0};
+    read(sock, buffer, 1024);
+    return std::string(buffer);
+}
 
-    void Client::terminate() {
-        close(sock);
-    }
+void Client::terminate() {
+    close(sock);
+}
