df4e102de535dced741ccb39bc4974e4ae943c07
lailahamdan1116 <94996494+lailahamdan1116@users.noreply.github.com>
2024-04-30 01:05:20 -0500

    Update client.cpp

--- ChatLML/client.cpp
+++ ChatLML/client.cpp
@@ -1,36 +1,31 @@
 #define PORT 8080
 
+#include "client.h"
 #include <iostream>
 #include <cstring>
-#include <string>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include "client.h"
-#include "server.h"
 
-Client::Client(std::string& user) : username(user), sock(-1) {
-    server_address.sin_family = AF_INET;  // Setting up default values
+Client::Client(const std::string& user) : username(user), sock(-1) {
+    server_address.sin_family = AF_INET;
     server_address.sin_port = htons(PORT);
     server_address.sin_addr.s_addr = INADDR_ANY;
 }
 
-    int Client::init(std::string IP) {
-    // Create socket
+int Client::init(std::string IP) {
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         std::cerr << "Socket creation error" << std::endl;
         exit(EXIT_FAILURE);
     }
 
-        // Initialize server address
-        if (inet_pton(AF_INET, IP.c_str(), &server_address.sin_addr) <= 0) {
+    if (inet_pton(AF_INET, IP.c_str(), &server_address.sin_addr) <= 0) {
         std::cerr << "Invalid address/ Address not supported" << std::endl;
         exit(EXIT_FAILURE);
     }
 
-      // Connect to server
     if (connect(sock, (struct sockaddr*)&server_address, sizeof(server_address)) < 0) {
         std::cerr << "Connection Failed" << std::endl;
         exit(EXIT_FAILURE);
@@ -43,7 +38,7 @@ void Client::send_message(const std::string& message) {
     send(sock, messageWithUsername.c_str(), messageWithUsername.length(), 0);
 }
 
-    std::string Client::receive_message() {
+std::string Client::receive_message() {
     char buffer[1024] = {0};
     read(sock, buffer, 1024);
     return std::string(buffer);
