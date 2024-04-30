ec587048df2a696a7220e576a624f95d2e6236ce
lailahamdan1116 <94996494+lailahamdan1116@users.noreply.github.com>
2024-04-30 12:11:51 -0500

    Update main.cpp

--- ChatLML/main.cpp
+++ ChatLML/main.cpp
@@ -9,37 +9,41 @@
 #include "client.h"
 #include "server.h"
 #include "../../../../usr/local/Cellar/fltk/1.3.9/include/FL/Fl.H"
+#include <iostream>
 
 // Forward declarations
 void host_loop(ChatBox chatbox);
 void client_loop(ChatBox chatbox);
 
-    int main() {
-
+    int main(int argc, char **argv) {
+    std::cout << "Creating window..." << std::endl;
     ChatBox chatBox(800, 800, 1000, 800, "ChatLML");
-    chatBox.show();
+    std::cout << "Showing window..." << std::endl;
+    chatBox.show(argc, argv);
+    std::cout << "Running event loop..." << std::endl;
     return Fl::run();
+}
     //Must get the username input from FL window
 
     //Call host_loop or join_loop depending on FL window input
-    return 0;
-}
+    //return 0;
+
 
 static void host_loop(ChatBox& chatbox, const std::string& username) {
     Server server(username);
     int new_socket = server.init();
     if (new_socket < 0) {
-        chatbox.addMessage("Chatroom creation failed");
+        chatbox.addMessage(username, "Chatroom creation failed");
         return;
     }
-    chatbox.addMessage("Chatroom successfully initialized!");
+    chatbox.addMessage(username, "Chatroom successfully initialized!");
 
     std::string rec = "", send = "", temp = "";
     while (true) {
         temp = server.receive_message(new_socket);
         if (rec != temp) {
             rec = temp;
-            chatbox.addMessage(rec);
+            chatbox.addMessage(username, rec);
         }
         // Example: get message to send
         // send = chatbox.getSendMessage();
@@ -58,18 +62,18 @@ static void join_loop(ChatBox chatbox,std::string username,std::string IP){
     std::string send = "";
     std::string temp = "";
     if(client.init(IP)!=0){ //Unsuccessful server initialization
-        chatbox.addMessage("Chatroom join failed");
+        chatbox.addMessage(username, "Chatroom join failed");
         return;
     }
     else{
-        chatbox.addMessage("Chatroom successfully initialized!");
+        chatbox.addMessage(username, "Chatroom successfully initialized!");
     }
     while(true){//Chatroom Loop
         //Check for new message
         temp = client.receive_message();
         if(rec!=temp){
             rec = temp;
-            chatbox.addMessage(rec);
+            chatbox.addMessage(username, rec);
         }
         //Send message
         //Need a method in chatbox which will tell main when send button is clicked and pass the input back
